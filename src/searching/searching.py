e226ee97498308f37f0581915e52e65d47b76cb5
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-18 14:40:05 -0700

    implement linear search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,9 +1,13 @@
 def linear_search(arr, target):
     # Your code here
+    for index in range(len(arr)):
+        
+        current_item = arr[index]
 
+        if target == current_item:
+            return index
 
     return -1   # not found
-
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
