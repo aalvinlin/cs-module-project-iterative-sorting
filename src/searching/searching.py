35af8112d643c29a9b48873bdd394cabef64ec93
Alvin Lin <57117545+aalvinlin@users.noreply.github.com>
2020-05-18 14:49:11 -0700

    begin implementing binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -12,7 +12,25 @@ def linear_search(arr, target):
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
 
-    # Your code here
+    lower_bound = 0
+    upper_bound = len(arr) - 1
 
+    while lower_bound <= upper_bound:
+
+        guess_index = (lower_bound + upper_bound) // 2
+        guess = arr[guess_index]
+
+        if guess == target:
+            return guess_index
+
+        # current midpoint is too small
+        # target would be found in the upper half of the range
+        elif guess < target:
+            lower_bound = guess_index + 1
+        
+        # current midpoint is too big
+        # target would be found in the lower half of the range
+        elif guess > target:
+            upper_bound = guess_index - 1
 
     return -1  # not found
